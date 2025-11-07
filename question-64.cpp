76153d3d41459c5ecb21ddc7f86d96cab5b57252
Saurabh Mishra <saurabh1805mishra@gmail.com>
2025-11-08 01:00:30 +0530

    Added question-64.cpp file

---
+++ question-64.cpp
@@ -0,0 +1,19 @@
+// pattern printing of alphabets like( a ab abc abcd abcde) in triangle form.
+
+#include <iostream>
+using namespace std;
+int main ()
+{
+    char c;
+
+  for(int i=1;i<6;i++)
+  {
+    c = 'A';
+    for(int j=1;j<=i;j++)
+    {
+        cout<<c<<" ";
+        c++;
+    }
+    cout<<endl;
+  }
+}
